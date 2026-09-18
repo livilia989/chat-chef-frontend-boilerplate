4e57ca09ee80f76d44b0f87f348d97e6813624bf
livili989 <livili989@gmail.com>
2026-09-18 11:04:26 +0900

    재반영

--- src/components/PrevButton.jsx
+++ src/components/PrevButton.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 const PrevButton = () => {
   // logic
-    const history = useNavigate()
-    
+  const history = useNavigate();
+
   const hadlePrev = () => {
-    console.log("이전 페이지로 이동");
+    history(-1);
   };
 
   // view
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,11 @@ import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  // logic'
-  const history = useNavigate()
+  // logic
+  const history = useNavigate();
 
   const handleStart = () => {
-    console.log("info페이지로 이동");
-    //react
+    // react-router-dom을 이용한 페이지 이동
     history("/info");
   };
 
